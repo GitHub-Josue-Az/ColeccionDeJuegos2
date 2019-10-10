7041de9b6094a036952246004c3c8c9304dfe26f
JosueAlva <josue.alva@tecsup.edu.pe>
2019-10-10 17:38:59 -0500

    Lab09

---
+++ ColeccionDeJuegos/JuegoViewController.swift
@@ -0,0 +1,88 @@
+//
+//  JuegoViewController.swift
+//  ColeccionDeJuegos
+//
+//  Created by JosueAlva on 10/9/19.
+//  Copyright © 2019 JosueAlva. All rights reserved.
+//
+
+import UIKit
+
+
+class JuegoViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+
+    
+    @IBOutlet weak var eliminarBoton: UIButton!
+    @IBOutlet weak var agregarActualizarBoton: UIButton!
+    @IBOutlet weak var JuegoImageView: UIImageView!
+    @IBOutlet weak var tituloTextField: UITextField!
+    var imagePicker = UIImagePickerController()
+    
+    var juego : Juego? = nil
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+        
+        if juego != nil{
+            JuegoImageView.image = UIImage(data: (juego!.imagen!) as Data)
+         tituloTextField.text = juego!.titulo
+         agregarActualizarBoton.setTitle("Actualizar", for: .normal)
+        }else{
+            eliminarBoton.isHidden = true
+        }
+        
+    }
+    
+    
+    @IBAction func fotosTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func camaraTapped(_ sender: Any) {
+        
+       imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    
+    @IBAction func agregarTapped(_ sender: Any) {
+        
+        
+        if juego != nil {
+            juego!.titulo = tituloTextField.text
+            juego!.imagen = JuegoImageView.image!.pngData() as NSData? as Data?
+        }else{
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            let juego = Juego(context: context)
+            juego.titulo = tituloTextField.text
+            juego.imagen = JuegoImageView.image!.pngData() as NSData? as Data?
+        }
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        navigationController!.popViewController(animated: true)
+        
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let imagenSeleccionada = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        JuegoImageView.image = imagenSeleccionada
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func eliminarTapped(_ sender: Any) {
+        
+ let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        context.delete(juego!)
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        navigationController!.popViewController(animated: true)
+        
+    }
+    
+    
+}
